657a1c11bada198aa011964381ef9d1df41515b2
blenhenok <blenhenok93@gmail.com>
2024-06-17 09:11:11 -0700

    starting my build fot fab motors website

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("myTopbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
